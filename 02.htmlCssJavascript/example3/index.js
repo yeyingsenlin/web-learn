635280cf21ea7c0e9d52565a2e40020df8ce4fc5
yeyingsenlin <yeyingsenlin928@qq.com>
2016-02-27 23:36:29 +0800

    Initial commit

---
+++ 02.htmlCssJavascript/example3/index.js
@@ -0,0 +1,16 @@
+function compute() {
+    var bt1=document.getElementById('num1');
+    var num1=parseInt(bt1.innerHTML);
+    var bt2=document.getElementById('num2');
+    var num2=parseInt(bt2.innerHTML);
+    alert(num1*num2);
+}
+
+var v = 3*5/2+(3232*3.221);
+console.log(v);
+
+console.log('txy i love you!');
+
+console.error('jskalfjsjfklsjkaf');
+
+console.warn('safdsafasf')
